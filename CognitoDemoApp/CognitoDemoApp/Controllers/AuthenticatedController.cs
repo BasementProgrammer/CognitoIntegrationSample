50578b677927808c7d69374d0500cca48ce5f3f6
Tom Moore <tom@basementprogrammer.com>
2023-05-12 13:12:58 -0400

    Initial checkin of sample code.

---
+++ CognitoDemoApp/CognitoDemoApp/Controllers/AuthenticatedController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CognitoDemoApp.Controllers
+{
+    [Authorize]
+    public class AuthenticatedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CognitoDemoApp/CognitoDemoApp/Program.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CognitoDemoApp
+{
+    public class Program
+    {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect(options =>
+            {
+                options.ResponseType = Configuration["Authentication:Cognito:ResponseType"];
+                options.MetadataAddress = Configuration["Authentication:Cognito:MetadataAddress"];
+                options.ClientId = Configuration["Authentication:Cognito:ClientId"];
+                options.Events = new OpenIdConnectEvents()
+                {
+                    OnRedirectToIdentityProviderForSignOut = OnRedirectToIdentityProviderForSignOut
+                };
+            });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+
+        private static Task OnRedirectToIdentityProviderForSignOut(RedirectContext context)
+        {
+            context.ProtocolMessage.Scope = "openid";
+            context.ProtocolMessage.ResponseType = "code";
+
+            var cognitoDomain = Configuration["Authentication:Cognito:CognitoDomain"];
+
+            var clientId = Configuration["Authentication:Cognito:ClientId"];
+
+            var logoutUrl = $"{context.Request.Scheme}://{context.Request.Host}{Configuration["Authentication:Cognito:AppSignOutUrl"]}";
+
+            context.ProtocolMessage.IssuerAddress = $"{cognitoDomain}/logout?client_id={clientId}&logout_uri={logoutUrl}&redirect_uri={logoutUrl}";
+
+            // delete cookies
+            context.Properties.Items.Remove(CookieAuthenticationDefaults.AuthenticationScheme);
+            // close openid session
+            context.Properties.Items.Remove(OpenIdConnectDefaults.AuthenticationScheme);
+
+            return Task.CompletedTask;
+        }
+
+    }
+}
